b8cae1f0f1ab7688ce87593cffbaa7425ddc2034
chris-kelly <chris_kelly@live.co.uk>
2022-09-04 21:17:54 +0100

    trying config options in Plotly

--- hello_world.js
+++ hello_world.js
@@ -1,8 +1,10 @@
 // https://github.com/looker-open-source/custom_visualizations_v2/blob/master/docs/api_reference.md
 // https://plotly.com/javascript/getting-started/
+// https://plotly.com/javascript/plotlyjs-function-reference/#plotlynewplot
 
 looker.plugins.visualizations.add({
-  // options for user to choose in the "edit" part of looker vis
+  // Options for user to choose in the "edit" part of looker vis
+  // In this example, whether the plot is bar or scatter
   options: { 
     graph_type: {
       type: "string",
@@ -19,10 +21,10 @@ looker.plugins.visualizations.add({
   // Set up the initial state of the visualization
   create: function(element, config) { 
 
-    // Insert a <style> tag with some styles we'll use later.
+    // Insert a <style> tag with class to keep stuff centered.
     element.innerHTML = `
       <style>
-        .hello-world-vis {
+        .container_style {
           /* Vertical centering */
           height: 100%;
           display: flex;
@@ -30,20 +32,14 @@ looker.plugins.visualizations.add({
           justify-content: center;
           text-align: center;
         }
-        .hello-world-text-large {
-          font-size: 72px;
-        }
-        .hello-world-text-small {
-          font-size: 18px;
-        }
       </style>
     `;
 
-    // Create a container element to let us center the text.
+    // Create the container element witht hat class to let us center the text.
     var container = element.appendChild(document.createElement("div"));
-    container.className = "hello-world-vis";
+    container.className = "container_style";
 
-    // import plotly script
+    // import plotly script to build that vis
     var script = document.createElement("script");
     script.type = "text/javascript";
     script.src = "https://cdn.plot.ly/plotly-2.14.0.min.js";
@@ -69,21 +65,31 @@ looker.plugins.visualizations.add({
     var x = []
     var y = []
 
-    for(var row of data) {
-			var x_i = row[queryResponse.fields.dimensions[0].name];
-      var y_i = row[queryResponse.fields.measures[0].name];
-			x.push(LookerCharts.Utils.textForCell(x_i));
-      y.push(LookerCharts.Utils.textForCell(y_i));
+    for(var row of data) { // for each row in data
+			var x_i = row[queryResponse.fields.dimensions[0].name]; // take first dimension
+      var y_i = row[queryResponse.fields.measures[0].name]; // take first measure
+			x.push(LookerCharts.Utils.textForCell(x_i)); // append to array
+      y.push(LookerCharts.Utils.textForCell(y_i)); // append to array
 		}
 
-    // Set the typr to the user-selected graph type
-
-    Plotly.newPlot( this._plotly_test, [{ 
+    data = [{  
       x: x,
       y: y,
-      type: config.graph_type
-    }], 
-    {margin: { t: 0 } } 
+      type: config.graph_type // Set the type to the user-selected graph type
+    }]
+
+    layout = {
+      margin: { t: 0 },
+      title: 'Click Here to Edit Chart Title'
+    }
+
+    config = {editable: true}
+
+    Plotly.newPlot( // use plotly library
+      this._plotly_test, // graphDiv
+      data,
+      layout,
+      config
     );
 
     // Let Looker know rendering is complete
