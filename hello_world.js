ed1356c69fa5e5204e7e7cd7129911e0db5c574b
chris-kelly <chris_kelly@live.co.uk>
2022-09-04 18:24:04 +0100

    go back to hello world

--- hello_world.js
+++ hello_world.js
@@ -1,14 +1,88 @@
+// https://github.com/looker/custom_visualizations_v2/blob/master/docs/api_reference.md
+
 looker.plugins.visualizations.add({
-	create: function(element, config){
-		element.innerHTML = "<h1>Ready to render!</h1>";
-	},
-	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-		var html = "";
-		for(var row of data) {
-			var cell = row[queryResponse.fields.dimensions[0].name];
-			html += LookerCharts.Utils.htmlForCell(cell);
-		}
-		element.innerHTML = html;
-		doneRendering()
-	}
+  // options for user to choose in the "edit" part of looker vis
+  options: { 
+    font_size: {
+      type: "string",
+      label: "Font Size",
+      values: [
+        {"Large": "large"},
+        {"Small": "small"}
+      ],
+      display: "radio",
+      default: "large"
+    }
+  },
+
+  // Set up the initial state of the visualization
+  create: function(element, config) { 
+
+    // Insert a <style> tag with some styles we'll use later.
+    element.innerHTML = `
+    <script async="" src="https://cdn.plot.ly/plotly-2.14.0.min.js" type="text/javascript" nonce=""></script>
+      <style>
+        .hello-world-vis {
+          /* Vertical centering */
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+        }
+        .hello-world-text-large {
+          font-size: 72px;
+        }
+        .hello-world-text-small {
+          font-size: 18px;
+        }
+      </style>
+      <div id="tester" style="width:600px;height:250px;"></div>
+      <script>
+        TESTER = document.getElementById('tester');
+        Plotly.newPlot( TESTER, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16] }], {
+        margin: { t: 0 } } );
+      </script>
+    `;
+
+    // Create a container element to let us center the text.
+    var container = element.appendChild(document.createElement("div"));
+    container.className = "hello-world-vis";
+
+    // Create an element to contain the text.
+    this._textElement = container.appendChild(document.createElement("div"));
+
+  },
+
+  
+  updateAsync: function(data, element, config, queryResponse, details, done) { // Update everytime data/settings change
+
+    // Clear errors from previous updates
+    this.clearErrors();
+
+    // Throw errors and exit if the shape of the data isn't what this chart requires
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({title: "No Dimensions", message: "This chart requires dimensions."}); // error
+      return; // exit
+    }
+
+    // Grab first cell of the data
+    var firstRow = data[0];
+    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+
+    // Insert the data into the page
+    this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
+
+    // Set the size to the user-selected size
+    if (config.font_size == "small") {
+      this._textElement.className = "hello-world-text-small";
+    } else {
+      this._textElement.className = "hello-world-text-large";
+    }
+
+    // Let Looker know rendering is complete
+    done()
+  }
 });
