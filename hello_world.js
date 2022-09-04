6e10e07305c4181a61a924d0414d16379fd24f6f
chris-kelly <chris_kelly@live.co.uk>
2022-09-04 18:01:46 +0100

    try random graph

--- hello_world.js
+++ hello_world.js
@@ -35,6 +35,14 @@ looker.plugins.visualizations.add({
           font-size: 18px;
         }
       </style>
+      <div id="tester" style="width:600px;height:250px;"></div>
+      <script>
+        TESTER = document.getElementById('tester');
+        Plotly.newPlot( TESTER, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16] }], {
+        margin: { t: 0 } } );
+      </script>
     `;
 
     // Create a container element to let us center the text.
