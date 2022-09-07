b0a2165af86336591e757b094b230ae161d47cd8
chris-kelly <chris_kelly@live.co.uk>
2022-09-07 08:03:46 +0100

    seeing what sticks

--- hello_world.js
+++ hello_world.js
@@ -52,13 +52,12 @@ looker.plugins.visualizations.add({
       </style>
     `;
 
-    // Create the container element witht hat class to let us center the text.
+    // Create the container element with that class to let us center the text.
     var container = element.appendChild(document.createElement("div"));
     container.className = "container_style";
 
-
     // Create an element to contain the plotly vis
-    this._plotly_test = container.appendChild(document.createElement("div"));
+    this._plotly_test = container//.appendChild(document.createElement("div"));
 
   },
 
