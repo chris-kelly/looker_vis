aa5ece4e2594d5cedff3104e073c546bbd56258d
chris-kelly <chris_kelly@live.co.uk>
2023-07-28 18:40:28 +0100

    index fix

--- scatter_plotly.js
+++ scatter_plotly.js
@@ -117,8 +117,8 @@ looker.plugins.visualizations.add({
 
       if (config.error_bands == true) {
           try {
+              mea.push(queryResponse.fields.table_calculations[0])
               mea.push(queryResponse.fields.table_calculations[1])
-              mea.push(queryResponse.fields.table_calculations[2])
               var y_lb = []; var y_ub = [];
           } 
           catch {
