aa700b6dfd963c2bfc8b62e742b393c3b4df942f
chris-kelly <chris_kelly@live.co.uk>
2023-08-02 09:39:36 +0100

    test

--- hello_world.js
+++ hello_world.js
@@ -6,9 +6,13 @@ looker.plugins.visualizations.add({
   // Options for user to choose in the "edit" part of looker vis
   // In this example, whether the plot is bar or scatter
   options: { 
+    // title: {
+    //   type: "string",
+    //   label: "1. Chart title",
+    // },
     plot_type: {
       type: "string",
-      label: "Plot type",
+      label: "2. Plot type",
       values: [
         {"Scatter": "scatter"},
         {"Bar": "bar"},
@@ -18,7 +22,7 @@ looker.plugins.visualizations.add({
     },
     scatter_mode: {
       type: "string",
-      label: "Plot type detail: scatter mode",
+      label: "2a. Plot type detail: scatter mode",
       values: [
         {"Markers": "markers"},
         {"Lines": "lines"},
@@ -29,7 +33,7 @@ looker.plugins.visualizations.add({
     },
     bar_mode: {
       type: "string",
-      label: "Plot type detail: bar mode",
+      label: "2b. Plot type detail: bar mode",
       values: [
         {"Grouped": "group"},
         {"Stacked": "stack"},
@@ -39,48 +43,53 @@ looker.plugins.visualizations.add({
     },
     error_bands: {
       type: "boolean",
-      label: "Add error bars?",
+      label: "3. Add error bars? (LB/UB is every 2nd/3rd col)",
+      default: false,
+    },
+    inverse_log: {
+      type: "boolean",
+      label: "4. Scale y by inverse log?",
       default: false,
     },
     value_labels: {
       type: "boolean",
-      label: "Labels: annotate?",
+      label: "5a. Format: Annotate labels?",
       default: false,
     },
     show_legend: {
-      type: "boolean",
-      label: "Show Legend?",
-      default: true,
+      type: "string",
+      label: "5b. Format: Display Legend?",
+      values: [
+        {"Hide": "z"},
+        {"Horizontal": "h"},
+        {"Vertical": "v"},
+      ],
+      default: "z",
     },
     xaxis_label: {
       type: "string",
-      label: "x axis: label",
+      label: "5c. Format: x axis label",
     },
     yaxis_label: {
       type: "string",
-      label: "y axis: label",
+      label: "5d. Format: y axis label",
     },
     xaxis_lim: {
       type: "string",
-      label: "x axis: manual limits (comma delimited)",
+      label: "5c. Format: x axis range (comma delimited)",
     },
     yaxis_lim: {
       type: "string",
-      label: "y axis: manual limits (comma delimited)",
+      label: "5d. Format: y axis range (comma delimited)",
     },
     xaxis_hover_format: {
       type: "string",
-      label: "x axis: manual format hover values",
+      label: "5c. Format: specify hover value format",
     },
     yaxis_hover_format: {
       type: "string",
-      label: "y axis: manual format hover values",
+      label: "5d. Format: specify hover value format",
     },
-    inverse_log: {
-      type: "boolean",
-      label: "Scale y by inverse log",
-      default: false,
-    }
   },
 
   // Set up the initial state of the visualization
@@ -136,61 +145,61 @@ looker.plugins.visualizations.add({
     // Clear errors from previous updates
     this.clearErrors();
     console.log(queryResponse) // see everything that is returned by Looker - just for debugging
-
-    let dim_names = queryResponse.fields.dimensions.map(d => d.name)
-    var mes_names = queryResponse.fields.measures.map(m => m.name)
-    var mes_names = mes_names.concat(queryResponse.fields.table_calculations.map(t => t.name)) // add table calcs to measures
-    
-    var mes_labels = queryResponse.fields.measures.map(m => m.label_short)
-    var mes_labels = mes_labels.concat(queryResponse.fields.table_calculations.map(t => t.label_short)) // get labels for nice formatting
-
-
-    if (queryResponse.fields.pivots.length > 0) {
-        var piv_keys = queryResponse.pivots.map(p => p.key)
-    }
-
-    // Throw errors and exit if the shape of the data isn't what this chart requires
-    if (dim_names.length < 1 || mes_names.length < 1) {
-      this.addError({title: "< 1 dimensions or measures.", message: "This chart requires at least two fields!"}); // error
-      return; // exit
-    }
     
     window.scriptLoad.then(() => { // Do this first to ensure js loads in time
 
-      var x = [], x_r = [], y = [], y_r = [], y_lab = [];
+      let dim_names = queryResponse.fields.dimensions.map(d => d.name) // dimension names
+      var mes_names = queryResponse.fields.measures.map(m => m.name) // measure names
+      var mes_names = mes_names.concat(queryResponse.fields.table_calculations.map(t => t.name)) // add table calcs to measures
+      var mes_labels = queryResponse.fields.measures.map(m => m.label_short) // get measure labels for nice formatting
+      var mes_labels = mes_labels.concat(queryResponse.fields.table_calculations.map(t => t.label_short)) 
+      if (queryResponse.fields.pivots.length > 0) { var piv_keys = queryResponse.pivots.map(p => p.key) } // get pivot names (if any)
+
+      // Throw errors and exit if the shape of the data isn't what this chart requires
+      if (dim_names.length < 1 || mes_names.length < 1) {
+        this.addError({title: "< 1 dimensions or measures.", message: "This chart requires at least two fields!"}); // error
+        return; // exit
+      }
+
+      // Function to try and find nicest rendered value for formatting
+      function get_pretty_data(d) {
+        if ('html' in d.keys()) { result = d.html } 
+        else if ('rendered' in d.keys()) { result = d.rendered } 
+        else { result = d.value }
+        return result
+      }
+
+      // loop through rows of data, create arrays of values and rendered (pretty) data 
+      var x = [], x_r = [], y = [], y_r = [];
+      
       for (var row of data) {
         x.push(dim_names.map(d => row[d].value).flat())
-        x_r.push(dim_names.map(d => row[d].html).flat())
-        if (piv_keys) {
+        x_r.push(dim_names.map(d => get_pretty_data(row[d])).flat())
+        if (piv_keys) { // if pivot, json has extra level, specified
           y.push(piv_keys.map(p => mes_names.map(m => row[m][p].value)).flat())
-          y_r.push(piv_keys.map(p => mes_names.map(m => row[m][p].html)).flat())
+          y_r.push(piv_keys.map(p => mes_names.map(m => get_pretty_data(row[m][p]))).flat())
         } else {
           y.push(mes_names.map(m => row[m].value).flat())
-          y_r.push(mes_names.map(m => row[m].html).flat())
+          y_r.push(mes_names.map(m => get_pretty_data(row[m])).flat())
         }
       }
 
-      console.log(y_r)
-
-      if (config.xaxis_label) { xaxis_label = config.xaxis_label} 
-      else { xaxis_label = queryResponse.fields.dimensions[0].label_short } // label axes
-
-      if (config.yaxis_label) { yaxis_label = config.yaxis_label} 
-      else { yaxis_label = queryResponse.fields.measures[0].label_short } // label axes
+      console.log(y_r) // for debugging
       
-      plotly_data = []
+      // if there are pivot keys, legend labels should include them
       legend_labels = [] 
       if (piv_keys) {
         for (var p of piv_keys) { for (var m of mes_labels) { legend_labels.push(p.replace('|FIELD|','').concat(' | ', m)) } }
       } else {
         for (var m of mes_labels) { legend_labels.push(m) }
       }
-
-      if (config.plot_type == 'scatter') {
-          var mode_type = config.scatter_mode
-      } else {
-          var mode_type = config.bar_mode
-      }
+      
+      if (config.plot_type == 'scatter') { var mode_type = config.scatter_mode} 
+      else { var mode_type = config.bar_mode }
+      
+      
+      // Loop over every measure and add as new trace
+      plotly_data = []
       
       for (var i = 0; i < y[0].length; i++) {
         
@@ -210,43 +219,48 @@ looker.plugins.visualizations.add({
         if (config.value_labels == true) {
           new_trace['textposition'] = 'top'
         }
-        if (config.error_bands == true) {
+        if (config.error_bands == true) { // if error bands, make every 2nd and 3rd column an error bar
             new_trace['error_y'] = {
                 type: 'data',
                 symmetric: false,
                 array: y.map(row => row[i+2]),
                 arrayminus: y.map(row => row[i+1]),
-              }
+            }
             i = i + 2
         }
         plotly_data.push(new_trace)
       }
 
-      console.log(plotly_data)
+      console.log(plotly_data) // for debugging
 
+      // set layout
+      if (config.xaxis_label) { xaxis_label = config.xaxis_label} // label axes
+      else { xaxis_label = queryResponse.fields.dimensions[0].label_short } 
+      if (config.yaxis_label) { yaxis_label = config.yaxis_label} 
+      else { yaxis_label = queryResponse.fields.measures[0].label_short } // label axes
       layout = {
         margin: { t: 0 },
         title: 'Click Here to Edit Chart Title',
         xaxis : {title: {text: xaxis_label}},
         yaxis : {title: {text: yaxis_label}},
-        showlegend: config.show_legend,
-        legend: {"orientation": "h"},
+        showlegend: false,
         colorway : ['#f3cec9', '#e7a4b6', '#cd7eaf', '#a262a9', '#6f4d96', '#3d3b72', '#182844']
       }
 
-      if (config.plot_type == 'bar') {
-        layout['barmode'] = mode_type
-      }
+      // Show legend and set orientation
+      if (config.show_legend != "z") { layout['showlegend'] = true; layout['legend'] = config.show_legend }
 
+      // set barmode is bar selected
+      if (config.plot_type == 'bar') { layout['barmode'] = mode_type}
+
+      // set limits for y and x axis
       if (config.xaxis_lim) {var xlim = config.xaxis_lim.split(","); layout['xaxis']['range'] = [Number(xlim[0]), Number(xlim[1])]}
       if (config.yaxis_lim) {var ylim = config.yaxis_lim.split(","); layout['yaxis']['range'] = [Number(ylim[0]), Number(ylim[1])]}
-      // if (config.xaxis_hover_format) {layout['xaxis']['hoverformat'] = config.xaxis_hover_format};
-      // if (config.yaxis_hover_format) {layout['yaxis']['hoverformat'] = config.yaxis_hover_format};
       
+      // Function to do inverse log
       if (config.inverse_log == true) {
 
         y = y.map(row => row[0])
-
         // create scaled version of y
         y_m = math.multiply( math.log10( 
           math.add( // equivalent to 1-y
@@ -260,11 +274,11 @@ looker.plugins.visualizations.add({
           -1)
         
         // Set hover-text with original value of y
-        plotly_data = [plotly_data[0]]
+        plotly_data = plotly_data[0]
         plotly_data['text'] = y
         plotly_data['hovertemplate'] = '<b>%{text}</b>'
-        // Overwrite y with scaled y
-        plotly_data['y'] = y_m._data
+        plotly_data['y'] = y_m._data // Overwrite y with scaled y
+        plotly_data = [plotly_data]
         
         // Overide yaxis labels
         layout['yaxis'] = {
@@ -278,8 +292,7 @@ looker.plugins.visualizations.add({
       }
 
       config = {
-        // editable: true, // allow user to edit axes by clicking on them
-        responsive: true
+        responsive: true // editable: true, // allow user to edit axes by clicking on them
       }
       
       Plotly.newPlot( // use plotly library
