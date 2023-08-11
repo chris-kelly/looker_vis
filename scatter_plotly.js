ede2504915742858677f00c811c23e3df3abb8aa
chris-kelly <chris_kelly@live.co.uk>
2023-08-11 17:47:27 +0100

    new & const for Map()

--- scatter_plotly.js
+++ scatter_plotly.js
@@ -297,7 +297,7 @@ looker.plugins.visualizations.add({
 
       console.log(queryResponse)
       
-      var nicedata = Map(), row0 = data[0] // use first row of data as blueprint. Use Map to preserve order
+      const nicedata = new Map(), row0 = data[0] // use first row of data as blueprint. Use Map to preserve order
       for (var k of Object.keys(row0)) {
         if (row0[k].hasOwnProperty('value')) { 
           // simply add keys of column if no pivot. Add name and "Label_short" or "label" data.
