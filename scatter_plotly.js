68dcae77d935ffca7563fcaa4e57391bcb830d86
chris-kelly <chris_kelly@live.co.uk>
2023-08-13 23:43:51 +0100

    hello

--- scatter_plotly.js
+++ scatter_plotly.js
@@ -311,10 +311,11 @@ looker.plugins.visualizations.add({
     for (i = 0; i < cols.length; i++) {
       options['div_' + i.toString()] = {type: "string", label: "<--- " + cols[i][0] + " --->", display: "divider", order: i*4}
       options['trace_' + i.toString()] = {type: "number", label: "Trace #", order: i*4+1, display_size: "third"}
-      options['axis_' + i.toString()] = {type: "string", label: "Axis", order: i*4+2, display_size: "third", display: 'select', values: [{"x1":'x1',"x2":'x2',"y1":'y1',"y2":'y2'}]}
-      options['type_' + i.toString()] = {type: "string", label: "Type", order: i*4+3, display_size: "third", display: 'select',values: [{"values":'values', "labels":'labels', "y lower bound": 'y_lb', "y upper bound": 'y_ub', "x lower bound": 'x_lb', "x upper bound": 'x_ub', "hovertext": 'hovertext'}]}
+      options['axis_' + i.toString()] = {type: "string", label: "Axis", order: i*4+2, display_size: "third", display: 'select', values: [{"x1":'x1'},{"x2":'x2'},{"y1":'y1'},{"y2":'y2'}]}
+      options['type_' + i.toString()] = {type: "string", label: "Type", order: i*4+3, display_size: "third", display: 'select', values: [{"values":'values'},{"labels":'labels'},{"y lower bound":'y_lb'},{"y upper bound":'y_ub'},{"x lower bound":'x_lb'},{"x upper bound":'x_ub'},{"hovertext":'hovertext'}]}
+      if (cols[i][1] == queryResponse.fields.dimension_like[0].name) { options['trace_' + i.toString()]['default'] = 1; options['axis_' + i.toString()]['default'] = "x1"; options['type_' + i.toString()]['default'] = "values" }
+      if (cols[i][1] == queryResponse.fields.measure_like[0].name) { options['trace_' + i.toString()]['default'] = 1; options['axis_' + i.toString()]['default'] = "y1"; options['type_' + i.toString()]['default'] = "values" }
     }
-    
     this.trigger('registerOptions', options)
     
     // for (i = 0; i < config.nt; i++) {
