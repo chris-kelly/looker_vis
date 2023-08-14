6ac90ed53d84bdd0b717dfcf8d80960753a82eae
chris-kelly <chris_kelly@live.co.uk>
2023-08-14 12:36:50 +0100

    hi

--- scatter_plotly.js
+++ scatter_plotly.js
@@ -6,7 +6,7 @@ looker.plugins.visualizations.add({
   // Options for user to choose in the "edit" part of looker vis
   // In this example, whether the plot is bar or scatter
   options: { 
-    nt: {type: number, label: "hello", default: 1},
+    nt: {type: "number", label: "hello", default: 1},
     add_trace: { type: "boolean", label: "flip me to hide the chart type", order: 1, default: false}
     // a01: {type: "string", label: "Axis", order: 0, display_size: "half", display: 'select', values: [{"-":null},{"x1":'x1'},{"y1":'y1'},{"x2":'x2'},{"y2":'y2'},{"x3":'x3'},{"y3":'y3'},{"x4":'x4'},{"y4":'y4'}], default: null, hidden: true},
     // t01: {type: "string", label: "Type", order: 0, display_size: "half", display: 'select', values: [{"-":null},{"values":'values'},{"labels":'labels'},{"y lower bound":'y_lb'},{"y upper bound":'y_ub'},{"x lower bound":'x_lb'},{"x upper bound":'x_ub'},{"hovertext":'hovertext'}], default: null, hidden: true},
@@ -329,11 +329,10 @@ looker.plugins.visualizations.add({
     // options.x1 = 
     // this.trigger('registerOptions', options)
 
-    const options = { ...this.options }; i = 0;
+    const options = { ...this.options };
     console.log(options)
     if (config.add_trace) {
-      ++options.nt
-      for (let i = 0; i < options.nt; i++) {
+      for (let i = 0; i < config.nt + 1; i++) {
         id = "x_" + i.toString()
         options[id] = {
           label: id,
@@ -343,6 +342,7 @@ looker.plugins.visualizations.add({
           values: cols,
         }
       }
+      ++options.nt
       options.add_trace = false
       // cols.forEach(x => {
       //   id = "test_" + Object.values(x);
