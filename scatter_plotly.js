16f7ce3d1d3087424621b21f750e7984b1888429
chris-kelly <chris_kelly@live.co.uk>
2023-08-15 16:59:26 +0100

    testing pivot-level formt

--- scatter_plotly.js
+++ scatter_plotly.js
@@ -64,13 +64,64 @@ looker.plugins.visualizations.add({
     window.scriptLoad.then(() => { // Do this first to ensure js loads in time
 
       ////////////////////////////////////////
-      // Get data to populate config:
-      ////////////////////////////////////////
-      
+      // Get data in nice format:
+      ////////////////////////////////////////    
+
+      console.log(queryResponse)
+
       function get_pretty_cols(d) {
         if (d.hasOwnProperty('label_short')) { result = d.label_short } else { result = d.label }
         return result
       }
+      // adapt column keys to get nicely formatted data in one string
+      function colname_format(field) {
+        var k = field.join(' ~ ').replace('|FIELD|',' | ').replace('$$$_row_total_$$$', 'ROW TOTAL')
+        return k
+      }
+        
+      // Get column names and metadata
+      var dim_names = queryResponse.fields.dimension_like.map(d => [d.name, get_pretty_cols(d)]) // retrieve both dimensions and non-pivotable table calcs, with nice labels too
+      var dimN = dim_names.map(d => d[0]), dimL = dim_names.map(d => d[1]);
+      var mes_names = queryResponse.fields.measure_like.map(m => [m.name, get_pretty_cols(m)]) // retrieve both measures and pivotable table calcs, with nice labels too
+      var mesN = mes_names.map(m => m[0]), mesL = mes_names.map(m => m[1]);
+      if (queryResponse.fields.pivots.length > 0) { var pivK = queryResponse.pivots.map(p => p.key) } // get pivot column names
+        
+      let nicedata = new Map(), row0 = data[0] // use first row of data as blueprint. Use Map for multiple keys
+      for (var k of Object.keys(row0)) {
+        if (row0[k].hasOwnProperty('value')) { 
+          // simply add keys of column if no pivot. Add name and "Label_short" or "label" data.
+          if (dimN.includes(k)) { nicedata.set([k], {'keys': [k], 'type': 'dimension', 'label': dimL[dimN.indexOf(k)] }) }
+          if (mesN.includes(k)) { nicedata.set([k], {'keys': [k], 'type': 'measure', 'label': mesL[mesN.indexOf(k)] }) }
+        } else { 
+          if (mesN.includes(k)) { // data includes hidden columns! So don't include these
+            var rowS = row0[k]; // the pivot (k2) is nested below each measure (k) in the data. Split these into seperate columns
+            for (var k2 of Object.keys(rowS)) {
+              if (k2 != '$$$_row_total_$$$') { // data may include row totals. Exclude for now
+                nicedata.set([k,k2], {'keys': [k,k2], 'type': 'pivot + measure', 'label': colname_format([k2, mesL[mesN.indexOf(k)]]) })
+              }
+            }
+          }
+        }
+      }
+
+      // make nice dict of values and another of labels/text
+      function get_pretty_data(d) {
+        if (d.hasOwnProperty('html')) { result = d.html } 
+        else if (d.hasOwnProperty('rendered')) { result = d.rendered } 
+        else { result = d.value }
+        return result
+      }
+      for (var k of [ ...nicedata.keys() ]) {
+        nicedata.set(k,{... nicedata.get(k), 
+          'values': data.map(row => k.length == 1 ? row[k[0]].value : row[k[0]][k[1]].value), // if two cols (due to pivot), i.e. length > 1, go into level below
+          'pretty': data.map(row => k.length == 1 ? get_pretty_data(row[k[0]]) : get_pretty_data(row[k[0]][k[1]]))
+        })
+      }
+      console.log(nicedata)      
+
+      ////////////////////////////////////////
+      // Get data to populate config:
+      ////////////////////////////////////////
 
       let cols = [{'-': ""}]
       queryResponse.fields.dimension_like.forEach(x => {d={};d[get_pretty_cols(x)]=x.name; cols.push(d)})
@@ -122,7 +173,21 @@ looker.plugins.visualizations.add({
         try {options["x_" + iN].default = queryResponse.fields.dimension_like[0].name} catch(err) {options["x_" + iN].default = Object.values(cols[0])}
         try {options["y_" + iN].default = queryResponse.fields.measure_like[i].name} catch(err) {options["y_" + iN].default = Object.values(cols[0])}
         try {options["col_" + iN].default = colors[i]} catch(err) {options["col_" + iN].default = colors[0]}
+
       }
+
+      if (queryResponse.fields.pivots.length > 0) {
+        options["intro_piv"] = {label: "NB - changes to pivot style overwrites traces", order: 0, type: "string", display: "divider", section: "Pivot styles"}
+        for (var piv of queryResponse.pivots.map(p => p.key)) {
+          options["p_div_" + piv] = {label: "<----------  " + piv + " ---------->", order: 5*i+1, type: "string", display: "divider", section: "Pivot styles"}
+          options["p_mod_" + piv] = {label: "Scatter mode " + iN2, order: 5*i+2, type: "string", display: "select", values: [{'Markers':'markers'},{'Lines':'lines'},{'Markers & Lines':'markers+lines'}], default: "markers", section: "Series"}
+          options["p_col_" + piv] = {label: "Colour: ", order: 5*i+3, type: "string", display: "color", section: "Series"}
+          options["p_sym_" + piv] = {label: "Symbol: ", order: 5*i+4, type: "string", display: "select", values: [
+            {'circle':'circle'},{'square':'square'},{'diamond':'diamond'},{'cross':'cross'},{'x':'x'},{'triangle': 'triangle'},{'pentagon':'pentagon'},{'hexagram':'hexagram'},{'star':'star'},{'hexagram':'hexagram'},{'hourglass':'hourglass'},{'bowtie':'bowtie'},{'asterisk':'asterisk'},{'hash':'hash'},{'y':'y'},{'line':'line'}
+          ], section: "Series"}
+        }
+      } else { for(let k of [...options.keys()]) { if (k.substring(0,2) == "p_") {delete options.i }} }
+
       var i = 0;
       for (xa of [...xax.values()]) { 
         options["xdiv_" + xa] = {label: "<---------- " + xa + " axis style ---------->", type: "string", display: "divider", section: "Style", default: "", order: i*13+1}
@@ -149,58 +214,6 @@ looker.plugins.visualizations.add({
       console.log(options)
       this.trigger('registerOptions', options)
       console.log(config)
-
-      ////////////////////////////////////////
-      // Get data in nice format:
-      ////////////////////////////////////////    
-
-      console.log(queryResponse)
-
-      // adapt column keys to get nicely formatted data in one string
-      function colname_format(field) {
-        var k = field.join(' ~ ').replace('|FIELD|',' | ').replace('$$$_row_total_$$$', 'ROW TOTAL')
-        return k
-      }
-        
-      // Get column names and metadata
-      var dim_names = queryResponse.fields.dimension_like.map(d => [d.name, get_pretty_cols(d)]) // retrieve both dimensions and non-pivotable table calcs, with nice labels too
-      var dimN = dim_names.map(d => d[0]), dimL = dim_names.map(d => d[1]);
-      var mes_names = queryResponse.fields.measure_like.map(m => [m.name, get_pretty_cols(m)]) // retrieve both measures and pivotable table calcs, with nice labels too
-      var mesN = mes_names.map(m => m[0]), mesL = mes_names.map(m => m[1]);
-      if (queryResponse.fields.pivots.length > 0) { var pivK = queryResponse.pivots.map(p => p.key) } // get pivot column names
-        
-      let nicedata = new Map(), row0 = data[0] // use first row of data as blueprint. Use Map for multiple keys
-      for (var k of Object.keys(row0)) {
-        if (row0[k].hasOwnProperty('value')) { 
-          // simply add keys of column if no pivot. Add name and "Label_short" or "label" data.
-          if (dimN.includes(k)) { nicedata.set([k], {'keys': [k], 'type': 'dimension', 'label': dimL[dimN.indexOf(k)] }) }
-          if (mesN.includes(k)) { nicedata.set([k], {'keys': [k], 'type': 'measure', 'label': mesL[mesN.indexOf(k)] }) }
-        } else { 
-          if (mesN.includes(k)) { // data includes hidden columns! So don't include these
-            var rowS = row0[k]; // the pivot (k2) is nested below each measure (k) in the data. Split these into seperate columns
-            for (var k2 of Object.keys(rowS)) {
-              if (k2 != '$$$_row_total_$$$') { // data may include row totals. Exclude for now
-                nicedata.set([k,k2], {'keys': [k,k2], 'type': 'pivot + measure', 'label': colname_format([k2, mesL[mesN.indexOf(k)]]) })
-              }
-            }
-          }
-        }
-      }
-
-      // make nice dict of values and another of labels/text
-      function get_pretty_data(d) {
-        if (d.hasOwnProperty('html')) { result = d.html } 
-        else if (d.hasOwnProperty('rendered')) { result = d.rendered } 
-        else { result = d.value }
-        return result
-      }
-      for (var k of [ ...nicedata.keys() ]) {
-        nicedata.set(k,{... nicedata.get(k), 
-          'values': data.map(row => k.length == 1 ? row[k[0]].value : row[k[0]][k[1]].value), // if two cols (due to pivot), i.e. length > 1, go into level below
-          'pretty': data.map(row => k.length == 1 ? get_pretty_data(row[k[0]]) : get_pretty_data(row[k[0]][k[1]]))
-        })
-      }
-      console.log(nicedata)
 
       ////////////////////////////////////////
       // Make plotly data
@@ -339,144 +352,9 @@ looker.plugins.visualizations.add({
         plotly_data,
         layout,
         {responsive: true}
-      )
-        
-        
-      //   var hovertemplate = "<b>%{xaxis.title.text}: </b> <br>%{x} <br>" + "<br>" + "<b>%{fullData.name}: </b> <br>%{text} <extra></extra>"
-      //   if (config.xaxis_hover_format) { hovertemplate = hovertemplate.replace("%{x}", "%{x:" + config.xaxis_hover_format + "}") }
-      //   if (config.yaxis_hover_format) { hovertemplate = hovertemplate.replace("%{text}", "%{y:" + config.yaxis_hover_format + "}") }
-      //   if (config.custom_hover_format) { hovertemplate = "%{text} <extra></extra>" }
-
-      //   var new_trace = {
-      //     x: x.map(row => row[0]),
-      //     y: y.map(row => row[i]),
-      //     type: config.plot_type,
-      //     mode: mode_type,
-      //     name: legend_labels[i],
-      //     text: y_r.map(row => row[i]),
-      //     textposition: "none",
-      //     hovertemplate: hovertemplate,
-      //   }
-      //   if (config.value_labels) {
-      //     if (config.plot_type == 'scatter') {
-      //       new_trace['mode'] = new_trace.mode + "+text"; new_trace['textposition'] = config.value_labels_pos_v + " " + config.value_labels_pos_h 
-      //     } else {
-      //       new_trace['textposition'] = config.value_labels_pos_b
-      //     }
-      //   } // Show values
-      //   if (config.value_labels_format) { new_trace['texttemplate'] = "%{y:" + config.value_labels_format + "}" }
-      //   if (config.error_bands == true) { // if error bands, make every 2nd and 3rd column an error bar
-      //       new_trace['error_y'] = {
-      //           type: 'data',
-      //           symmetric: false,
-      //           array: y.map(row => row[i+2]),
-      //           arrayminus: y.map(row => row[i+1]),
-      //       }
-      //       i = i + 2
-      //   }
-      //   plotly_data.push(new_trace)
-      // }
-
-      // console.log(plotly_data) // for debugging
-
-      // // set layout
-      // if (config.xaxis_label) { xaxis_label = config.xaxis_label} // label axes
-      // else { xaxis_label = get_pretty_labels(queryResponse.fields.dimensions[0]) } 
-      // if (config.yaxis_label) { yaxis_label = config.yaxis_label} 
-      // else { yaxis_label = get_pretty_labels(queryResponse.fields.measures[0]) } // label axes
-
-      // // set colours
-      // if (config.colorPreSet  == 'c') { var colorSettings =  config.colorRange || ['#f3cec9', '#e7a4b6', '#cd7eaf', '#a262a9', '#6f4d96', '#3d3b72', '#182844'] }// put a default in
-      // else { var colorSettings =  config.colorPreSet.split(",");}
-
-      // layout = {
-      //   margin: { 
-      //     l: parseInt(config.margins.split(',')[0]),
-      //     r: parseInt(config.margins.split(',')[1]),
-      //     b: parseInt(config.margins.split(',')[2]),
-      //     t: parseInt(config.margins.split(',')[3]),
-      //     pad: parseInt(config.margins.split(',')[4]),
-      //   },
-      //   title: config.graph_title,
-      //   xaxis : {title: {text: xaxis_label}, automargin: true},
-      //   yaxis : {title: {text: yaxis_label}, automargin: true},
-      //   showlegend: false,
-      //   colorway : colorSettings
-      // }
-
-      // // Show legend and set orientation
-      // if (config.show_legend != "z") { layout['showlegend'] = true; layout['legend'] = {"orientation": config.show_legend} }
-
-      // // set barmode is bar selected
-      // if (config.plot_type == 'bar') { layout['barmode'] = mode_type}
-
-      // // set limits for y and x axis
-      // if (config.xaxis_lim) {var xlim = config.xaxis_lim.split(","); layout['xaxis']['range'] = [Number(xlim[0]), Number(xlim[1])]}
-      // if (config.yaxis_lim) {var ylim = config.yaxis_lim.split(","); layout['yaxis']['range'] = [Number(ylim[0]), Number(ylim[1])]}
-      
-      // if (config.swap_axes) {
-      //   for (var i = 0; i < plotly_data.length; i++) {
-      //     x = structuredClone(plotly_data[i]['x'])
-      //     y = structuredClone(plotly_data[i]['y'])
-      //     plotly_data[i]['x'] = y
-      //     plotly_data[i]['y'] = x
-      //     plotly_data[i]['error_x'] = structuredClone(plotly_data[i]['error_y'])
-      //     delete plotly_data[i].error_y
-      //   }
-      //   xaxis = structuredClone(layout['xaxis'])
-      //   yaxis = structuredClone(layout['yaxis'])
-      //   layout['yaxis'] = xaxis
-      //   layout['xaxis'] = yaxis
-      // }
-
-      // // Function to do inverse log
-      // if (config.inverse_log == true) {
-
-      //   yn = y.map(row => row[0])
-      //   // create scaled version of y
-      //   y_m = math.multiply( math.log10( 
-      //     math.add( // equivalent to 1-y
-      //         1,
-      //         math.multiply( 
-      //           math.matrix(yn), // convert y to mathjs vector
-      //           -1
-      //           )
-      //         )
-      //       ),
-      //     -1)
-        
-      //   // Set hover-text with original value of y
-      //   plotly_data = plotly_data[0]
-      //   plotly_data['text'] = y
-      //   plotly_data['hovertemplate'] = '<b>%{text}</b>'
-      //   plotly_data['y'] = y_m._data // Overwrite y with scaled y
-      //   plotly_data = [plotly_data]
-        
-      //   // Overide yaxis labels
-      //   layout['yaxis'] = {
-      //     title: {text: yaxis_label},
-      //     tickmode: 'array',
-      //     tickvals: [0,1,2,3],
-      //     ticktext: [0,0.9,0.99,0.999],
-      //     range: [0,3.1]
-      //   }
-
-      // }
-
-      // config = {
-      //   responsive: true // editable: true, // allow user to edit axes by clicking on them
-      // }
-      
-      // Plotly.newPlot( // use plotly library
-      //   this.plotly_bit, // graphDiv
-      //   plotly_data,
-      //   layout,
-      //   config
-      // )
-      
+      )      
     })
 
-    // Let Looker know rendering is complete
-    done()
+    done() // Let Looker know rendering is complete
   }
 });
